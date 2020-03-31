7e5b585ea41e952338300e3a0e564146115306ea
Alexandre Petitjean <alpetitjean@gmail.com>
2020-03-31 22:54:41 +0200

    Add windows installer

---
+++ cmake/WinDeployQt.cmake
@@ -0,0 +1,65 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Nathan Osman
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+find_package(Qt5Core REQUIRED)
+
+# Retrieve the absolute path to qmake and then use that path to find
+# the windeployqt binary
+get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+
+# Add commands that copy the Qt runtime to the target's output directory after
+# build and install the Qt runtime to the specified directory
+function(windeployqt target directory)
+
+    # install(CODE ...) doesn't support generator expressions, but
+    # file(GENERATE ...) does - store the path in a file
+    file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${target}"
+            CONTENT "$<TARGET_FILE:${target}>"
+            )
+
+    # Before installation, run a series of commands that copy each of the Qt
+    # runtime files to the appropriate directory for installation
+    install(CODE
+            "
+        file(READ \"${CMAKE_CURRENT_BINARY_DIR}/${target}\" _file)
+        execute_process(
+            COMMAND \"${CMAKE_COMMAND}\" -E
+                env PATH=\"${_qt_bin_dir}\" \"${WINDEPLOYQT_EXECUTABLE}\"
+                    --no-compiler-runtime
+                    --no-angle
+                    --no-opengl-sw
+                    --dir \${CMAKE_INSTALL_PREFIX}/${directory}
+                    \${_file}
+            OUTPUT_VARIABLE _output
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        "
+            )
+    #    set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
+    #    set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION ${CMAKE_INSTALL_PREFIX}/${directory})
+    #    include(InstallRequiredSystemLibraries)
+
+endfunction()
+
+mark_as_advanced(WINDEPLOYQT_EXECUTABLE)
