213f3ba578e534deec32f9251949e4df4029b23b
Alexandre Petitjean <alpetitjean@gmail.com>
2021-05-12 08:02:06 +0200

    Fix HR translation

---
+++ src/app/l10n/kemai_hr.ts
@@ -0,0 +1,307 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr">
+<context>
+    <name>ActivityDialog</name>
+    <message>
+        <location filename="../activitydialog.ui" line="14"/>
+        <source>Activity</source>
+        <translation>Aktivnost</translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="25"/>
+        <source>0</source>
+        <translation>0</translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="35"/>
+        <source>Budget</source>
+        <translation>Proračun</translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="62"/>
+        <source>Name</source>
+        <translation>Ime</translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="85"/>
+        <source>Time Budget</source>
+        <translation>Vremenski proračun</translation>
+    </message>
+</context>
+<context>
+    <name>ActivityWidget</name>
+    <message>
+        <location filename="../activitywidget.ui" line="14"/>
+        <source>ActivityWidget</source>
+        <translation>Programčić aktivnosti</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="29"/>
+        <source>Customer</source>
+        <translation>Kupac</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="60"/>
+        <location filename="../activitywidget.ui" line="110"/>
+        <location filename="../activitywidget.ui" line="160"/>
+        <source>...</source>
+        <translation>…</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="79"/>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="129"/>
+        <source>Activity</source>
+        <translation>Aktivnost</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="173"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="183"/>
+        <source>Tags</source>
+        <translation>Oznake</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="193"/>
+        <source>Started at</source>
+        <translation>Pokrenuto</translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="240"/>
+        <source>00:00:00</source>
+        <translation>00:00:00</translation>
+    </message>
+</context>
+<context>
+    <name>CustomerDialog</name>
+    <message>
+        <location filename="../customerdialog.ui" line="14"/>
+        <source>Customer</source>
+        <translation>Kupac</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="25"/>
+        <source>Country</source>
+        <translation>Zemlja</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="55"/>
+        <source>Currency</source>
+        <translation>Valuta</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="62"/>
+        <source>Name</source>
+        <translation>Ime</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="69"/>
+        <source>Time Budget</source>
+        <translation>Vremenski proračun</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="93"/>
+        <source>Timezone</source>
+        <translation>Vremenska zona</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="103"/>
+        <source>Budget</source>
+        <translation>Proračun</translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="126"/>
+        <source>0</source>
+        <translation>0</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Kemai - Kimai client</source>
+        <translation>Kemai – Kimai klijent</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="48"/>
+        <source>Settings...</source>
+        <translation>Postavke …</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectDialog</name>
+    <message>
+        <location filename="../projectdialog.ui" line="14"/>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="25"/>
+        <source>Name</source>
+        <translation>Ime</translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="32"/>
+        <source>Budget</source>
+        <translation>Proračun</translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="68"/>
+        <source>Time Budget</source>
+        <translation>Vremenski proračun</translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="75"/>
+        <source>0</source>
+        <translation>0</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="../settingswidget.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Postavke</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="25"/>
+        <source>Host</source>
+        <translation>Host</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="39"/>
+        <source>Username</source>
+        <translation>Korisničko ime</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="49"/>
+        <source>Token</source>
+        <translation>Token</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="63"/>
+        <source>Language</source>
+        <translation>Jezik</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="70"/>
+        <source>Close to system tray</source>
+        <translation>Zatvori u programsku traku</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="104"/>
+        <source>Test</source>
+        <translation>Testiraj</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="124"/>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="131"/>
+        <source>Save</source>
+        <translation>Spremi</translation>
+    </message>
+</context>
+<context>
+    <name>kemai::app::MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="35"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Zatvori program</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="36"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Postavke</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Check for updates...</source>
+        <translation>Traži nove verzije …</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="38"/>
+        <source>Open Kimai instance</source>
+        <translation>Otvori primjerak Kemaia</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="58"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datoteka</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="63"/>
+        <source>&amp;Help</source>
+        <translation>Po&amp;moć</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="68"/>
+        <source>About Qt</source>
+        <translation>Qt informacije</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="181"/>
+        <source>New version available</source>
+        <translation>Postoji nova verzija</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="182"/>
+        <source>Version %1 is available.
+
+%2</source>
+        <translation>Dostupna je verzija %1.
+
+%2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>No update</source>
+        <translation>Nema nove verzije</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>%1 is latest version.</source>
+        <translation>%1 je najnovija verzija.</translation>
+    </message>
+</context>
+<context>
+    <name>kemai::app::SettingsWidget</name>
+    <message>
+        <location filename="../settingswidget.cpp" line="57"/>
+        <source>Language changed. Application restart is required.</source>
+        <translation>Jezik je promijenjen. Program se mora ponovo pokrenuti.</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.cpp" line="91"/>
+        <source>Invalid reply.</source>
+        <translation>Neispravan odgovor.</translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.cpp" line="96"/>
+        <source>Connected to Kimai %1</source>
+        <translation>Povezano s Kimai %1</translation>
+    </message>
+</context>
+<context>
+    <name>kemai::client::KimaiClient::KimaiClientPrivate</name>
+    <message>
+        <location filename="../src/core/client/kimaiclient.cpp" line="37"/>
+        <source>Error on request [%1]: %2</source>
+        <translation>Greška u zahtjevu [%1]: %2</translation>
+    </message>
+    <message>
+        <location filename="../src/core/client/kimaiclient.cpp" line="48"/>
+        <source>Unknown request [%1]</source>
+        <translation>Nepoznat zahtjev [%1]</translation>
+    </message>
+</context>
+</TS>
