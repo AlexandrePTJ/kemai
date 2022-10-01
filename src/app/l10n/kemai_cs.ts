c8c9b136a79beddf866d6b988dfcc4daeebe3e73
office256 <ohofman@seznam.cz>
2022-10-01 16:14:23 +0200

    Added translation using Weblate (Czech)

---
+++ src/app/l10n/kemai_cs.ts
@@ -0,0 +1,346 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>ActivityDialog</name>
+    <message>
+        <location filename="../activitydialog.ui" line="14"/>
+        <source>Activity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="25"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="35"/>
+        <source>Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="62"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitydialog.ui" line="85"/>
+        <source>Time Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ActivityWidget</name>
+    <message>
+        <location filename="../activitywidget.ui" line="14"/>
+        <source>ActivityWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="29"/>
+        <source>Customer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="60"/>
+        <location filename="../activitywidget.ui" line="110"/>
+        <location filename="../activitywidget.ui" line="160"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="79"/>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="129"/>
+        <source>Activity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="173"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="183"/>
+        <source>Tags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="193"/>
+        <source>Started at</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../activitywidget.ui" line="240"/>
+        <source>00:00:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CustomerDialog</name>
+    <message>
+        <location filename="../customerdialog.ui" line="14"/>
+        <source>Customer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="25"/>
+        <source>Country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="55"/>
+        <source>Currency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="62"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="69"/>
+        <source>Time Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="93"/>
+        <source>Timezone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="103"/>
+        <source>Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../customerdialog.ui" line="126"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Kemai - Kimai client</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="48"/>
+        <source>Settings...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProjectDialog</name>
+    <message>
+        <location filename="../projectdialog.ui" line="14"/>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="25"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="32"/>
+        <source>Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="68"/>
+        <source>Time Budget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../projectdialog.ui" line="75"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="../settingswidget.ui" line="14"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="25"/>
+        <source>Host</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="39"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="49"/>
+        <source>Token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="63"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="70"/>
+        <source>Close to system tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="110"/>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="130"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.ui" line="137"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TaskWidget</name>
+    <message>
+        <location filename="../taskwidget.ui" line="14"/>
+        <source>TaskWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../taskwidget.ui" line="25"/>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../taskwidget.ui" line="35"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../taskwidget.ui" line="63"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../taskwidget.ui" line="73"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>kemai::app::MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="38"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="40"/>
+        <source>Check for updates...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Open Kimai instance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="42"/>
+        <source>Activities</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="43"/>
+        <source>Tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="72"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="77"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="81"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="86"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="293"/>
+        <source>New version available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="293"/>
+        <source>Version %1 is available.
+
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="315"/>
+        <source>No update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="315"/>
+        <source>%1 is latest version.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>kemai::app::SettingsWidget</name>
+    <message>
+        <location filename="../settingswidget.cpp" line="57"/>
+        <source>Language changed. Application restart is required.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.cpp" line="78"/>
+        <source>Invalid reply.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingswidget.cpp" line="83"/>
+        <source>Connected to Kimai %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>kemai::client::KimaiClient::KimaiClientPrivate</name>
+    <message>
+        <source>Error on request [%1]: %2</source>
+        <translation type="vanished">Erreur sur la requête [%1] : %2</translation>
+    </message>
+    <message>
+        <source>Unknown request [%1]</source>
+        <translation type="vanished">Requête inconnue [%1]</translation>
+    </message>
+</context>
+</TS>
